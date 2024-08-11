70da845d5db386b8045815b2a5399d943aa3ce81
lucasspor <lucassporto.contato@gmail.com>
2024-08-11 14:30:47 -0300

    Feat: Adding select, input and button for create a activity

--- html_css_javascript/script.js
+++ html_css_javascript/script.js
@@ -1,10 +1,32 @@
 
+const formatador = (data) => {
+  return {
+    dia: {
+      numerico: dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd')
+      }
+    },
+    mes: dayjs(data).format('MMMM'),
+    horas: dayjs(data).format('HH:mm')
+  }
+}
+
 const atividade = {
   nome: "Almoço",
   data: new Date("2024-07-08 10:00"),
   finalizada: true
 }
-let atividades = [];
+let atividades = [
+  atividade,
+  {
+    nome: "Academia em grupo",
+    data: new Date("2024-07-08 12:00"),
+    finalizada: false
+
+  }
+];
 
 const criarItemDeAtividade = (atividade) => {
 
@@ -16,18 +38,19 @@ const criarItemDeAtividade = (atividade) => {
 
   input += '>';
 
+  const formatar = formatador(atividade.data);
+
   return `<div>
       ${input}
       <span>${atividade.nome}</span>
-      <time>${atividade.data}</time>
-
+      <time>${formatar.dia.semana.longo}, dia ${formatar.dia.numerico} de ${formatar.mes} às ${formatar.horas}</time>
     </div>`
 }
 
 const atualizarListaDeAtividades = () => {
   const section = document.querySelector("section");
 
-  if(atividades.length == 0){
+  if (atividades.length == 0) {
     section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`
     return
   }
