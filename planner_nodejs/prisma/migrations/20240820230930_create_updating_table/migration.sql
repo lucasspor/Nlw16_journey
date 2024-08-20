8c7bdf4595b032d3478b27be6d1428cd54a1d040
lucasspor <lucassporto.contato@gmail.com>
2024-08-20 20:12:03 -0300

    Add: adding nodejs api using prisma and SQLite for a planner

---
+++ planner_nodejs/prisma/migrations/20240820230930_create_updating_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "trips" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "destination" TEXT NOT NULL,
+    "starts_at" DATETIME NOT NULL,
+    "ends_at" DATETIME NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "participant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "is_owner" BOOLEAN NOT NULL DEFAULT false,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "participant_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
---
+++ planner_nodejs/src/routes/create-trip.ts
@@ -0,0 +1,86 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import dayjs from 'dayjs'
+import { getMailClient } from "../lib/mail";
+import nodemailer from 'nodemailer'
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/trips', {
+    schema: {
+      body: z.object({
+        destination: z.string().min(4),
+        starts_at: z.coerce.date(),
+        ends_at: z.coerce.date(),
+        owner_name: z.string(),
+        owner_email: z.string().email(),
+        email_to_invite: z.array(z.string().email()),
+      })
+    }
+  }, async (request) => {
+    const { destination, starts_at, ends_at, owner_name, owner_email, email_to_invite } = request.body
+
+    if (dayjs(starts_at).isBefore(new Date())) {
+      throw new Error('Invalid trip start date')
+    }
+
+    if (dayjs(ends_at).isBefore(starts_at)) {
+      throw new Error('Invalid trip end date')
+    }
+
+    const trip = await prisma.trip.create(
+      {
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data:[
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_confirmed: true,
+                  is_owner: true
+                },
+                ...email_to_invite.map(email => {
+                  return {email}
+                })
+              ]
+            }
+          }
+        }
+      }
+    )
+
+    const formateDateStart = dayjs("12-25-1995", "MM-DD-YYYY")
+
+    const formateDateEnd = dayjs("12-25-1995", "MM-DD-YYYY")
+
+    const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`
+
+    const mail = await getMailClient()
+
+    const message = await mail.sendMail({
+      from: {
+        name: 'Equipe plann.er',
+        address: 'oi@plann.er',
+      },
+      to: {
+        name: owner_name,
+        address: owner_email,
+      },
+      subject: '<h1>Testando Email</h1>',
+      html: '<p>teste do envio</p>',
+    })
+
+    console.log(nodemailer.getTestMessageUrl(message))
+
+    return { tripId: trip.id }
+  })
+} 
