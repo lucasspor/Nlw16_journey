f275df24d5514777c0f577c9f40013a6f4e92f25
lucasspor <lucassporto.contato@gmail.com>
2024-08-20 10:31:50 -0300

    feat: adding DatePicker and Format Date for my locale

--- plann.er_reactjs/src/pages/create-trip/steps/destination-and-date-step.tsx
+++ plann.er_reactjs/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,4 +1,8 @@
+import { ptBR } from "date-fns/locale";
+import "react-datepicker/dist/react-datepicker.css";
 import { MapPin, Calendar, ArrowRight, Settings2   } from "lucide-react";
+import { useState } from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
 
 interface DestinationAndDateStepProps{
   isGuestsInputOpen: boolean
@@ -6,7 +10,14 @@ interface DestinationAndDateStepProps{
   openGuestsInput: () => void
 }
 
+type DateRange = [Date | undefined, Date | undefined];
+
 export function DestinationAndDateStep({isGuestsInputOpen, closeGuestsInput,openGuestsInput}: DestinationAndDateStepProps){
+  registerLocale('pt-BR', ptBR)
+
+  const [dateRange, setDateRange] = useState<DateRange>([undefined, undefined])
+  const [startDate, endDate] = dateRange;
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
     <div className='flex gap-2 items-center flex-1'>
@@ -15,7 +26,18 @@ export function DestinationAndDateStep({isGuestsInputOpen, closeGuestsInput,open
     </div>
     <div className='flex gap-2 items-center '>
       <Calendar className='size-5 text-zinc-400' />
-      <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 outline-none w-32" />
+      <DatePicker   
+      selectsRange={true}
+      startDate={startDate}
+      endDate={endDate}
+      onChange={(update: [Date | null, Date | null]) => {
+        setDateRange(update.map(date => date || undefined) as DateRange);
+      }}
+      isClearable={true}
+      locale="pt-BR"
+      dateFormat="dd 'a' dd 'de' MMM." disabled={isGuestsInputOpen} placeholderText="Quando?" 
+      className="bg-transparent text-lg placeholder-zinc-400 outline-none w-52"
+      />
     </div>
 
     <div className='w-px h-6 bg-zinc-800'></div>
